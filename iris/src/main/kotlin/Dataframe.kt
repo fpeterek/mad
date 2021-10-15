7b8fd6daa98fee69fa789ad2cdc9cc5488b2724f
Filip Peterek <filip.peterek@firma.seznam.cz>
2021-10-15 21:17:56 +0200

    Median, avg instance

--- iris/src/main/kotlin/Dataframe.kt
+++ iris/src/main/kotlin/Dataframe.kt
@@ -18,13 +18,22 @@ class Dataframe<T> private constructor(val data: List<T>) {
 
     fun avg(of: T.() -> Double) = data.sumOf(of) / data.size
 
-    fun avgString(of: T.() -> String) {
-        val counts = mutableMapOf<String, Long>()
+    fun <T2> median(of: T.() -> T2, cmp: Comparator<T2>): T2 {
+        val counts = mutableMapOf<T2, Long>()
         iterator(of).forEach {
-            counts[it] = counts.getOrDefault(it, 0) + 1
+            counts[it] = counts.getOrDefault(it, 0L) + 1L
         }
-    }
+        val keys = counts.keys.toList().sortedWith(cmp)
+        val size = data.size
+        var current = 0L
+        var i = 0
+        while (current < size/2) {
+            current += counts[keys[i]!!]!!
+            i += 1
+        }
 
+        return keys[i]
+    }
 
     private fun varianceOf(item: T, of: T.() -> Double, avg: Double) =
         (item.of() - avg).pow(2)
--- iris/src/main/kotlin/main.kt
+++ iris/src/main/kotlin/main.kt
@@ -12,9 +12,24 @@ fun main() {
         )
     }
 
-    val avgSepalWidth = df.avg(IrisRecord::sepalWidth)
-    val swVariance = df.totalVariance(IrisRecord::sepalWidth, avgSepalWidth)
+    val avgInstance = IrisRecord(
+        sepalLength = df.avg(IrisRecord::sepalLength),
+        sepalWidth = df.avg(IrisRecord::sepalWidth),
+        petalLength = df.avg(IrisRecord::petalLength),
+        petalWidth = df.avg(IrisRecord::petalWidth),
+        variety = df.median(IrisRecord::variety, String::compareTo),
+    )
 
-    println("Avg=$avgSepalWidth; variance=$swVariance")
+    val slVariance = df.totalVariance(IrisRecord::sepalLength, avgInstance.sepalLength)
+    val swVariance = df.totalVariance(IrisRecord::sepalWidth, avgInstance.sepalWidth)
+    val plVariance = df.totalVariance(IrisRecord::petalLength, avgInstance.petalLength)
+    val pwVariance = df.totalVariance(IrisRecord::petalWidth, avgInstance.petalWidth)
+
+    println("Average instance: $avgInstance")
+
+    println("Sepal length variance: $slVariance")
+    println("Sepal width variance: $swVariance")
+    println("Petal length variance: $plVariance")
+    println("Petal width variance: $pwVariance")
 
 }
