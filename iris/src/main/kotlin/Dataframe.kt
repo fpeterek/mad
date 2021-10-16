dec66dba0e9728bd1f2b857044884eb8d7d4f951
Filip Peterek <filip.peterek@firma.seznam.cz>
2021-10-16 19:41:06 +0200

    Pdf, cdf, plots

--- iris/src/main/kotlin/Dataframe.kt
+++ iris/src/main/kotlin/Dataframe.kt
@@ -1,6 +1,8 @@
 package org.fpeterek.mad.iris
 
+import kotlin.math.exp
 import kotlin.math.pow
+import kotlin.math.sqrt
 
 class Dataframe<T> private constructor(val data: List<T>) {
 
@@ -35,6 +37,41 @@ class Dataframe<T> private constructor(val data: List<T>) {
         return keys[i]
     }
 
+    fun min(of: T.() -> Double) = iterator(of).asSequence().minByOrNull { it } ?:
+        throw RuntimeException("Empty dataframe")
+
+    fun max(of: T.() -> Double) = iterator(of).asSequence().maxByOrNull { it } ?:
+        throw RuntimeException("Empty dataframe")
+
+    private fun pdf(value: Double, avg: Double, variance: Double) =
+        (sqrt(2*Math.PI*variance)).pow(-1) * exp(-0.5 * (value-avg).pow(2) / variance)
+
+    fun distribution(of: T.() -> Double, samples: Int = 100) =
+        (min(of) to max(of)).let { (minVal, maxVal) ->
+            val diff = maxVal - minVal
+            val avgVal = avg(of)
+            val variance = totalVariance(of, withAvg = avgVal)
+            (0..samples).associate {
+                val value = minVal + (it.toDouble() / samples) * diff
+                value to pdf(value, avgVal, variance)
+            }
+        }
+
+    fun cumulativeDistribution(of: T.() -> Double, samples: Int = 100) =
+        distribution(of, samples).let {  dist ->
+            val keys = dist.keys.sorted()
+            val values = mutableMapOf(
+                keys.first() to dist[keys.first()]!!
+            )
+            keys.indices.drop(1).forEach {
+                values[keys[it]] = (values[keys[it-1]]!! + dist[keys[it]]!!)
+            }
+
+            val max = values[keys.last()]!!
+
+            values.mapValues { (_, it) -> it / max }
+        }
+
     private fun varianceOf(item: T, of: T.() -> Double, avg: Double) =
         (item.of() - avg).pow(2)
 
@@ -44,9 +81,10 @@ class Dataframe<T> private constructor(val data: List<T>) {
     fun totalVariance(of: T.() -> Double, withAvg: Double? = null) =
         (withAvg ?: avg(of)).let { average ->
             data
+                .asSequence()
                 .map { varianceOf(it, of, average) }
                 .let {
-                    it.sum() / it.size
+                    it.sum() / data.size
                 }
         }
 
--- iris/src/main/kotlin/main.kt
+++ iris/src/main/kotlin/main.kt
@@ -1,16 +1,66 @@
 package org.fpeterek.mad.iris
 
+import jetbrains.letsPlot.letsPlot
+import jetbrains.datalore.plot.PlotSvgExport
+import jetbrains.letsPlot.geom.geomLine
+import jetbrains.letsPlot.intern.toSpec
+import java.awt.Desktop
+import java.io.File
+
+
+fun plotFunction(
+    df: Dataframe<IrisRecord>,
+    fn: Dataframe<IrisRecord>.(IrisRecord.() -> Double) -> Map<Double, Double>,
+    of: IrisRecord.() -> Double,
+    outFile: String) {
+
+    val slDist = df.fn(of).toList()
+    val slPlotValues = mapOf(
+        "range" to slDist.map { it.first },
+        "probability" to slDist.map { it.second },
+    )
+
+    val plot = letsPlot(slPlotValues) {
+        x = "range"
+        y = "probability"
+    } + geomLine(color = "black", alpha=0.8, size=1.0)
+
+    val content = PlotSvgExport.buildSvgImageFromRawSpecs(plot.toSpec())
+    val file = File(outFile)
+    file.createNewFile()
+    file.writeText(content)
+    Desktop.getDesktop().browse(file.toURI())
+
+}
+
+
+fun plotDist(df: Dataframe<IrisRecord>, of: IrisRecord.() -> Double, outFile: String) = plotFunction(
+    df,
+    Dataframe<IrisRecord>::distribution,
+    of,
+    outFile,
+)
+
+fun plotCumulative(df: Dataframe<IrisRecord>, of: IrisRecord.() -> Double, outFile: String) = plotFunction(
+    df,
+    Dataframe<IrisRecord>::cumulativeDistribution,
+    of,
+    outFile,
+)
+
+fun loadIrisDf(filename: String) = Dataframe.from(filename) {
+    IrisRecord(
+        it[0].toDouble(),
+        it[1].toDouble(),
+        it[2].toDouble(),
+        it[3].toDouble(),
+        it[4],
+    )
+}
+
 fun main() {
 
-    val df = Dataframe.from("iris.csv") {
-        IrisRecord(
-            it[0].toDouble(),
-            it[1].toDouble(),
-            it[2].toDouble(),
-            it[3].toDouble(),
-            it[4],
-        )
-    }
+    val df = loadIrisDf("iris.csv")
 
     val avgInstance = IrisRecord(
         sepalLength = df.avg(IrisRecord::sepalLength),
@@ -24,6 +74,16 @@ fun main() {
     val swVariance = df.totalVariance(IrisRecord::sepalWidth, avgInstance.sepalWidth)
     val plVariance = df.totalVariance(IrisRecord::petalLength, avgInstance.petalLength)
     val pwVariance = df.totalVariance(IrisRecord::petalWidth, avgInstance.petalWidth)
+
+    plotDist(df, IrisRecord::sepalLength, "slDistribution.svg")
+    plotDist(df, IrisRecord::sepalWidth, "swDistribution.svg")
+    plotDist(df, IrisRecord::petalLength, "plDistribution.svg")
+    plotDist(df, IrisRecord::petalWidth, "pwDistribution.svg")
+
+    plotCumulative(df, IrisRecord::sepalLength, "slCumulative.svg")
+    plotCumulative(df, IrisRecord::sepalWidth, "swCumulative.svg")
+    plotCumulative(df, IrisRecord::petalLength, "plCumulative.svg")
+    plotCumulative(df, IrisRecord::petalWidth, "pwCumulative.svg")
 
     println("Average instance: $avgInstance")
 
