2c260e8736c5b24e52237dc4ea22643b804da573
Filip Peterek <filip.peterek@firma.seznam.cz>
2021-12-25 20:45:40 +0100

    Nullability fixes, basic stats

--- austin-crime/src/main/scala/org/fpeterek/mad/crimeanalysis/CrimeProcessor.scala
+++ austin-crime/src/main/scala/org/fpeterek/mad/crimeanalysis/CrimeProcessor.scala
@@ -1,9 +1,9 @@
 package org.fpeterek.mad.crimeanalysis
 
-import org.apache.spark.sql.types.{BinaryType, DoubleType, IntegerType, LongType, StringType, StructType}
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType, StringType, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.fpeterek.mad.crimeanalysis.util.SparkUtil.kryoEncoder
-import org.joda.time.LocalDate
+import org.fpeterek.mad.crimeanalysis.util.SparkUtil.{RowUtils, kryoEncoder, withCached}
 import org.joda.time.format.DateTimeFormat
 
 object CrimeProcessor {
@@ -27,39 +27,42 @@ class CrimeProcessor(private val spark: SparkSession) extends Serializable {
 
   private def schema = new StructType()
     .add("primaryKey", LongType, nullable=false)
-    .add("councilDistrict", IntegerType, nullable=false)
+    .add("councilDistrict", IntegerType, nullable=true)
     .add("highestOffense", StringType, nullable=false)
     .add("crimeType", StringType, nullable=false)
     .add("reportDate", DateType, nullable=false)
-    .add("location", StringType, nullable=false)
-    .add("xCoordinate", DoubleType, nullable=false)
-    .add("yCoordinate", DoubleType, nullable=false)
-    .add("clearanceStatus", StringType, nullable=false)
-    .add("clearanceDate", DateType, nullable=false)
+    .add("location", StringType, nullable=true)
+    .add("xCoordinate", DoubleType, nullable=true)
+    .add("yCoordinate", DoubleType, nullable=true)
+    .add("clearanceStatus", StringType, nullable=true)
+    .add("clearanceDate", DateType, nullable=true)
     .add("goDistrict", StringType, nullable=false)
-    .add("goZipCode", IntegerType, nullable=false)
-    .add("goCensusTract", DoubleType, nullable=false)
+    .add("goZipCode", IntegerType, nullable=true)
+    .add("goCensusTract", DoubleType, nullable=true)
 
   private def getDF(filename: String): DataFrame = {
     val rdd = spark.read.csv(filename)
       .map { row =>
-        val reportDate = LocalDate.parse(row.getString(4), dateFormatter)
-        val clearanceDate = LocalDate.parse(row.getString(9), dateFormatter)
+        val reportDate = row.dateOrNull(4, dateFormatter)
+        val clearanceDate = row.dateOrNull(9, dateFormatter)
 
         Row(
           row.getString(0).toLong,
-          row.getString(1).toInt,
+          row.intOrNull(1),
           row.getString(2),
           row.getString(3),
           Row(reportDate.getYear, reportDate.getMonthOfYear, reportDate.getDayOfMonth),
           row.getString(5),
-          row.getString(6).toDouble,
-          row.getString(7).toDouble,
+          row.doubleOrNull(6),
+          row.doubleOrNull(7),
           row.getString(8),
-          Row(clearanceDate.getYear, clearanceDate.getMonthOfYear, clearanceDate.getDayOfMonth),
+          clearanceDate match {
+            case null => null
+            case _ => Row(clearanceDate.getYear, clearanceDate.getMonthOfYear, clearanceDate.getDayOfMonth)
+          },
           row.getString(10),
-          row.getString(11).toInt,
-          row.getString(12).toDouble
+          row.intOrNull(11),
+          row.doubleOrNull(12)
         )
       }
       .rdd
@@ -67,11 +70,29 @@ class CrimeProcessor(private val spark: SparkSession) extends Serializable {
     spark.createDataFrame(rdd, schema)
   }
 
-  def process(filename: String): Unit = {
-    val df = getDF(filename)
+  private def mostCommon(df: DataFrame, column: String) = df
+    .groupBy(col(column))
+    .count()
+    .collect()
+
+  private def mostCommonOffense(df: DataFrame) = mostCommon(df, "highestOffense")
+  private def mostCommonCrime(df: DataFrame) = mostCommon(df, "crimeType")
+  private def mostCommonMonth(df: DataFrame) = mostCommon(df, "reportDate.month")
+  private def mostCommonDistrict(df: DataFrame) = mostCommon(df, "councilDistrict")
+  private def mostCommonClearanceMonth(df: DataFrame) = mostCommon(df, "clearanceDate.month")
+  private def mostCommonStatus(df: DataFrame) = mostCommon(df, "clearanceStatus")
+
+  def process(filename: String): Unit = withCached(getDF(filename)) { df: DataFrame =>
 
     df.printSchema()
     println(df.take(10).mkString("Array(", ", ", ")"))
+
+    println(mostCommonOffense(df).mkString("Array(", ", ", ")"))
+    println(mostCommonCrime(df).mkString("Array(", ", ", ")"))
+    println(mostCommonMonth(df).mkString("Array(", ", ", ")"))
+    println(mostCommonDistrict(df).mkString("Array(", ", ", ")"))
+    println(mostCommonClearanceMonth(df).mkString("Array(", ", ", ")"))
+    println(mostCommonStatus(df).mkString("Array(", ", ", ")"))
   }
 
 }
--- austin-crime/src/main/scala/org/fpeterek/mad/crimeanalysis/util/SparkUtil.scala
+++ austin-crime/src/main/scala/org/fpeterek/mad/crimeanalysis/util/SparkUtil.scala
@@ -1,7 +1,9 @@
 package org.fpeterek.mad.crimeanalysis.util
 
-import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Encoder, Row, SparkSession}
 import org.apache.spark.storage.StorageLevel
+import org.joda.time.LocalDate
+import org.joda.time.format.DateTimeFormatter
 
 object SparkUtil {
 
@@ -11,18 +13,36 @@ object SparkUtil {
   implicit def kryoEncoder[A](implicit ct: ClassTag[A]): Encoder[A] =
     org.apache.spark.sql.Encoders.kryo[A](ct)
 
-  def withCached[T](ds: Dataset[T], level: StorageLevel): (Dataset[T] => Unit) => Unit =  {
+  implicit class RowUtils(row: Row) {
+    def doubleOrNull(idx: Int): java.lang.Double = row.isNullAt(idx) match {
+      case true => null
+      case false => row.getString(idx).toDouble
+    }
 
-    def cachedFun(fn: Dataset[T] => Unit): Unit = {
+    def intOrNull(idx: Int): java.lang.Integer = row.isNullAt(idx) match {
+      case true => null
+      case false => row.getString(idx).toInt
+    }
+
+    def dateOrNull(idx: Int, formatter: DateTimeFormatter): LocalDate = row.isNullAt(idx) match {
+      case true => null
+      case false => LocalDate.parse(row.getString(idx), formatter)
+    }
+  }
+
+  def withCached[DT, RT](ds: Dataset[DT], level: StorageLevel): (Dataset[DT] => RT) => RT =  {
+
+    def cachedFun(fn: Dataset[DT] => RT): RT = {
       val cached = ds.persist(level)
-      fn(cached)
+      val res = fn(cached)
       cached.unpersist()
+      res
     }
 
     cachedFun
   }
 
-  def withCached[T](ds: Dataset[T]): (Dataset[T] => Unit) => Unit = withCached(ds, StorageLevel.MEMORY_AND_DISK)
+  def withCached[DT, RT](ds: Dataset[DT]): (Dataset[DT] => RT) => RT = withCached(ds, StorageLevel.MEMORY_AND_DISK)
 
   implicit class CloseableSpark(ss: SparkSession) {
     def use[T](fn: SparkSession => T): T = try {
